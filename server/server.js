6c4322c3475c16fc613d8f9f6d58d4b406c3c0b6
DaQuan Cartwright <daquancartwright@outlook.com>
2023-07-27 11:53:39 -0700

    Added Rollbar Integration

--- server/server.js
+++ server/server.js
@@ -4,4 +4,16 @@ const app = express()
 app.use(express.static(`public`))
 app.use(express.static(`${__dirname}/public`))
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '35d730f83c0448ffbd940aad1d7b35aa',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
+
 app.listen(4000, () => console.log(`server running on 4000`))
